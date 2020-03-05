5367274639f934b2ceca86846ea6fc9c5d9d580f
Benjamin <benjamin@stenberg-jakobsen.dk>
2020-03-05 14:00:45 +0100

    Add structure

---
+++ app/src/Screens/HomeScreen/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.css';
+
+function HomeScreen(props){
+  return (
+    <>
+    </>
+  );
+}
+
+export default HomeScreen;
---
+++ app/src/Screens/InteractiveScreen/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.css';
+
+function InteractiveScreen(props){
+  return (
+    <>
+    </>
+  );
+}
+
+export default InteractiveScreen;

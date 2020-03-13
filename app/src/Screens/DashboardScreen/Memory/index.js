1af293efa11d54a68ae2cdf601432cfb5dd2e3ed
Benjamin Stenberg Jakobsen <benjamin@stenberg-jakobsen.dk>
2020-03-13 01:11:45 +0100

    state update memory

--- app/src/Screens/DashboardScreen/Memory/index.js
+++ app/src/Screens/DashboardScreen/Memory/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './index.css';
 import TopMenu from './TopMenu';
 import {Route} from 'react-router-dom';
@@ -7,10 +7,22 @@ import MemoryPalace from './MemoryPalace';
 import Flashcard from './Flashcard';
 
 function Memory(props){
-  return (
-    <>
-      <TopMenu/>
+    var [render, setRender] = useState(true);
+  
+  if(render){
+    return( 
+      <>
+      <div onClick={()=> setRender(false)}>
+        <TopMenu />
+      </div>
 
+      <div id = "Header-container" >
+        <h1>Welcome to the memory page</h1>
+      </div>
+      <div>
+        <h2>Choose category above to get started!</h2>
+      </div>
+      
       <Route exact path = {`/dashboard/memory/flashcards`}>
         <Flashcard/>
       </Route>
@@ -20,9 +32,23 @@ function Memory(props){
       <Route exact path = {`/dashboard/memory/dual_n_back`}>
         <DualNBack/>
       </Route>
-      
     </>
-  );
-}
+  );}
 
+  else{
+    return( 
+      <>
+        <TopMenu/>
+      <Route exact path = {`/dashboard/memory/flashcards`}>
+        <Flashcard/>
+      </Route>
+      <Route exact path = {`/dashboard/memory/memory_palace`}>
+        <MemoryPalace/>
+      </Route>
+      <Route exact path = {`/dashboard/memory/dual_n_back`}>
+        <DualNBack/>
+      </Route>
+    </>
+    );}
+}
 export default Memory;

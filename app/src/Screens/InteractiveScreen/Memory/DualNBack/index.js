d95dfb2c6206fb434deb9d225117e46d05bcdc92
Benjamin Stenberg Jakobsen <benjamin@stenberg-jakobsen.dk>
2020-03-25 15:52:52 +0100

    dual_n_back random color

--- app/src/Screens/InteractiveScreen/Memory/DualNBack/index.js
+++ app/src/Screens/InteractiveScreen/Memory/DualNBack/index.js
@@ -1,21 +1,42 @@
 import React, { useState } from 'react';
 import './index.css';
-import TopMenu from '../TopMenu'
 import Dropdown from 'react-bootstrap/Dropdown'
+
+var indexes = [false, false, false, false, false, false, false, false, false]
+var rounds = 10;
+
+function game(){
+  console.log(rounds);
+  for(var i = 0; i < 9; i++){
+    document.getElementById(i).style.backgroundColor = "white";
+  }
+
+  if(rounds > 0){
+    document.getElementById(Math.ceil(Math.random() * 9) - 1).style.backgroundColor = "green";
+    rounds -= 1;
+    console.log(indexes);
+    setTimeout(game, 1000);
+  } 
+  else{
+    return(true);
+  }
+}
 
 function DualNBack(props){
   var [N, setN] = useState(0);
   var [audio, setAudio] = useState(false);
   var [position, setPosition] = useState(false);
+  var [start, setStart] = useState(true);
+  var time = 0;
 
   return (
     <>
     <div id = "dropdown-container">
       <Dropdown>
-        <Dropdown.Toggle variant="primary">
+        <Dropdown.Toggle  className="dropdown-class" variant="primary">
           Choose your N
         </Dropdown.Toggle>
-        <Dropdown.Menu>
+        <Dropdown.Menu className ="dropdown-menu">
           <Dropdown.Item onSelect={() => {setN(2)}}>2</Dropdown.Item>
           <Dropdown.Item onSelect={() => {setN(3)}}>3</Dropdown.Item>
           <Dropdown.Item onSelect={() => {setN(4)}}>4</Dropdown.Item>
@@ -23,8 +44,33 @@ function DualNBack(props){
       </Dropdown>
     </div>
 
-    <div id = "game-container">
+    <div className = "wrapper">
+      <div>
+        {start ? [
+          <div onClick = {() => 
+            {setStart(false)
+              game()
+            }}>
+          <spam className="start-btn-text">Click to start!</spam>
+          <div className = "overlay"></div>
+        </div>
+        ] : null}
+      </div>
+      
+      <div className = "game-container">
+        <div id = "0" className = "grid-item"> </div>
+        <div id = "1" className = "grid-item"> </div>
+        <div id = "2" className = "grid-item"> </div>
+        <div id = "3" className = "grid-item"> </div>
+        <div id = "4" className = "grid-item"> </div>
+        <div id = "5" className = "grid-item"> </div>
+        <div id = "6" className = "grid-item"> </div>
+        <div id = "7" className = "grid-item"> </div>
+        <div id = "8" className = "grid-item"> </div>
+      </div>
     </div>
+
+    
 
     <div className = "dual-button-class" id = "audio-container" 
     onClick = {() => {setAudio(true)}}>

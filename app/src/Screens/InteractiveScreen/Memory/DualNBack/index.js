50544c4168c87631d69f157a580b8c3e7cb64a43
Benjamin Stenberg Jakobsen <benjamin@stenberg-jakobsen.dk>
2020-04-01 15:33:24 +0200

    dual-n-back functionality

--- app/src/Screens/InteractiveScreen/Memory/DualNBack/index.js
+++ app/src/Screens/InteractiveScreen/Memory/DualNBack/index.js
@@ -2,38 +2,70 @@ import React, { useState } from 'react';
 import './index.css';
 import Dropdown from 'react-bootstrap/Dropdown'
 
-var indexes = [false, false, false, false, false, false, false, false, false]
-var rounds = 10;
+var rounds = 20;
+var order = [[], []];
+var n = 2;
+var audio = false;
+var position = false;
+var score = [0, 0];
+
+function setN(newN){
+  n = newN;
+}
+function setPosition(pos){
+  position = pos;
+}
+function setAudio(newAudio){
+  audio = newAudio;
+}
 
 function game(){
-  console.log(rounds);
+  console.log(score);
   for(var i = 0; i < 9; i++){
     document.getElementById(i).style.backgroundColor = "white";
   }
 
   if(rounds > 0){
-    document.getElementById(Math.ceil(Math.random() * 9) - 1).style.backgroundColor = "green";
-    rounds -= 1;
-    console.log(indexes);
-    setTimeout(game, 1000);
-  } 
+    setTimeout(function(){
+      var pos = Math.ceil(Math.random() * 9) - 1;
+      document.getElementById(pos).style.backgroundColor = "rgb(255, 195, 11)";
+      order[0].unshift(pos);
+      console.log(order[0]);
+      rounds -= 1;
+      setTimeout(checkPos, 950);
+      setTimeout(game, 1000);}, 200);
+  }
   else{
-    return(true);
+    return (true);
   }
 }
 
+function checkPos(){
+  if(n > order[0].length && position == true){
+    score[1] += 1;
+  }
+  else if(order[0].length >= n){
+    if(order[0][n] == order[0][0] && position == true){
+      score[0] += 1;
+    }
+    else if(order[0][n] == order[0][0] && position == false){
+      score[1] += 1;
+    }
+    else if(order[0][n] != order[0][0] && position == true){
+      score[1] += 1;
+    }
+  }
+  setPosition(false);
+}
+
 function DualNBack(props){
-  var [N, setN] = useState(0);
-  var [audio, setAudio] = useState(false);
-  var [position, setPosition] = useState(false);
   var [start, setStart] = useState(true);
-  var time = 0;
 
   return (
-    <>
+    <div className = "background">
     <div id = "dropdown-container">
       <Dropdown>
-        <Dropdown.Toggle  className="dropdown-class" variant="primary">
+        <Dropdown.Toggle  className="dropdown-class" variant="warning">
           Choose your N
         </Dropdown.Toggle>
         <Dropdown.Menu className ="dropdown-menu">
@@ -68,9 +100,7 @@ function DualNBack(props){
         <div id = "7" className = "grid-item"> </div>
         <div id = "8" className = "grid-item"> </div>
       </div>
-    </div>
-
-    
+    </div>    
 
     <div className = "dual-button-class" id = "audio-container" 
     onClick = {() => {setAudio(true)}}>
@@ -81,11 +111,7 @@ function DualNBack(props){
     onClick = {() => {setPosition(true)}}>
       Position 
     </div>
-
-    {/*setAudio(false), setPosition(false)*/}
-    {console.log(N)}
-
-    </>
+    </div>
   );
 }
 
